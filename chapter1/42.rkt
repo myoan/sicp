4679eacf542ce850cca3cfde1ef2f54f24c77c81
myoan <motoki.yoan@gmail.com>
2023-06-18 12:55:15 +0900

    Solve 1.43

--- chapter1/42.rkt
+++ chapter1/42.rkt
@@ -3,11 +3,4 @@
 (define (square x) (* x x))
 (define (inc x) (+ x 1))
 
-(define (repeat f n)
-	(define (iter f g n)
-		(if (= n 0)
-			(lambda (x) (g x))
-			(iter f (compose f g) (- n 1))))
-	(iter f (lambda (x) x) n))
-
-((repeat square 2) 5)
+((compose square inc) 6)
