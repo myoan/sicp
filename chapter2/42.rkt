95e1595776ed9fa0a6c8c407b217ec141cb189f1
myoan <motoki.yoan@gmail.com>
2023-07-02 17:43:18 +0900

    refactor 2.42

--- chapter2/42.rkt
+++ chapter2/42.rkt
@@ -5,8 +5,7 @@
 (define (queens board-size)
 	(define (queen-cols k)
 		(if (= k 0)
-			; (list empty-board)
-			(list (list null))
+			(list null)
 			(filter
 				(lambda (positions) (safe? k positions))
 				(flatmap
@@ -18,34 +17,22 @@
 	(queen-cols board-size))
 
 (define (adjoin-position row col rest-of-queens) 
-	(filter
-		(lambda (x) (not (null? x)))
-		(append rest-of-queens (list (list row col)))))
+	(cons (list row col) rest-of-queens))
 
 (define (safe? k poses)
-	; (display "safe? ")
-	; (print poses) (newline)
 	(define (hit-line? src dst dx dy)
-		; (display "try hit-line: ") (print src) (display " to ") (print dst) (display ": (") (print dx) (display ", ") (print dy) (display ")") (newline)
 		(cond
 			((or (< (car src) 1) (< (+ k 1) (car src))) #f)
 			((or (< (cadr src) 1) (< (+ k 1) (cadr src))) #f)
-			; ((or (< (car src) 0) (< k (car src))) (display "x < 0 or k < x") (newline) #f)
-			; ((or (< (cadr src) 0) (< k (cadr src))) (display "y < 0 or k < y") (newline) #f)
 			((and (= (car src) (car dst)) (= (cadr src) (cadr dst)))
-				; (display "hit-line: ") (print src) (display " to ") (print dst) (display ": (") (print dx) (display ", ") (print dy) (display ")") (newline)
 				#t)
 			(else (hit-line? (list (+ (car src) dx) (+ (cadr src) dy)) dst dx dy))))
 	(define (hit? src dst)
-		; (display "compare: ")
-		; (print src) (print dst) (newline)
 		(or
-			(hit-line? src dst -1 1)
-			(hit-line? src dst 0 1)
-			(hit-line? src dst 1 1)))
+			(hit-line? src dst -1 -1)
+			(hit-line? src dst 0 -1)
+			(hit-line? src dst 1 -1)))
 	(define (safe-iter src others)
-		; (display "safe-iter: ")
-		; (print src) (display ": ") (print others) (newline)
 		(cond
 			((null? others) #t)
 			(else (and
@@ -66,6 +53,6 @@
 	(print (queens n)) (newline))
 
 ; (display-queens-result 3)
-; (display-queens-result 4)
+(display-queens-result 4)
 ; (display-queens-result 5)
-(display-queens-result 8)
+; (display-queens-result 8)
